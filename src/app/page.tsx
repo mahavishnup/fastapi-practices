781c3f786b37086ae1c688dd5b03b8b92fa6c4a0
Mahavishnu P <selvamvishnu25@gmail.com>
2024-04-09 13:36:17 +0530

    feat: cloudflare url added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,9 @@ export default function Home() {
       // once deployed, prefix this with your cloudflare worker url
       // i.e.: https://<name>.<account-name>.workers.dev/api/search?q=${input}
 
-      const res = await fetch(`/api/search?q=${input}`)
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_CLOUDFLARE_WORKERS_URL}/api/search?q=${input}`,
+      )
       const data = (await res.json()) as {
         results: string[]
         duration: number
